5d29ccfb41981bfefe188cb6d1dea8291caa10bb
Ibnul Ashir <ibnulashir42@gmail.com>
2021-09-25 20:53:07 +0600

    resume section completed

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,13 +1,17 @@
 import './Resume.css';
 import React from 'react';
-import { Grid, Typography } from '@mui/material';
+import { Grid, Icon, Paper, Typography } from '@mui/material';
 import resumeData from '../../utils/resumeData';
+import CustomTimeline, { CustomTimelineSeparator } from '../../components/Timeline/Timeline';
+import WorkIcon from '@mui/icons-material/Work';
+import { TimelineContent, TimelineDot, TimelineItem } from '@mui/lab';
+import SchoolIcon from '@mui/icons-material/School';
 
 const Resume = () => {
     return (
         <>
             {/* about me */}
-            <Grid container className="section pb_45" >
+            <Grid container className="section pb_10 pt_25 pl_30" >
                 <Grid item className="section_title mb_30">
                     <span></span>
                     <Typography variant="h5" className="section_title_text">About Me</Typography>
@@ -18,13 +22,112 @@ const Resume = () => {
             </Grid>
 
             {/* education */}
-            <Grid container="" >
-
+            <Grid container className="section pb_10 pt_25 pl_30" >
+                <Grid item className="section_title mb_30">
+                        <span></span>
+                    <Typography variant="h5" className="section_title_text">Resume</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid container className="resume_timeline">
+                        <Grid item sm={12} md={6}>
+                            <CustomTimeline title="Work Experience" icon={<WorkIcon />}>
+                                {
+                                    resumeData.experiences.map(experience => {
+                                        return(
+                                            <TimelineItem>
+                                                <CustomTimelineSeparator />
+                                            <TimelineContent>
+                                                <Typography className="titmeline_title">
+                                                    {experience.title}
+                                                </Typography>
+                                                <Typography variant="body2" className="timeline_date">
+                                                    {experience.date}
+                                                </Typography>
+                                                <Typography className="timeline_description">
+                                                    {experience.description}
+                                                </Typography>
+                                            </TimelineContent>
+                                            </TimelineItem>
+                                        )                                        
+                                    })
+                                }
+                            </CustomTimeline>
+                        </Grid>
+                        <Grid item sm={12} md={6}>
+                        <CustomTimeline title="Education" icon={<SchoolIcon />}>
+                                {
+                                    resumeData.education.map(education => {
+                                        return(
+                                            <TimelineItem>
+                                                <CustomTimelineSeparator />
+                                            <TimelineContent>
+                                                <Typography className="titmeline_title">
+                                                    {education.title}
+                                                </Typography>
+                                                <Typography variant="body2" className="timeline_date">
+                                                    {education.date}
+                                                </Typography>
+                                                <Typography className="timeline_description">
+                                                    {education.description}
+                                                </Typography>
+                                            </TimelineContent>
+                                            </TimelineItem>
+                                        )                                        
+                                    })
+                                }
+                            </CustomTimeline>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Grid>
+            
+            {/* services */}
+            <Grid container className="section pb_10 pt_25 pl_30" >
+                <Grid item className="section_title mb_30">
+                    <span></span>
+                    <Typography variant="h5" className="section_title_text">My Services</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid container spacing={3} justifyContent='space-around'>
+                        {
+                            resumeData.services.map(service => (
+                                <Grid item xs={12} sm={6} mb={3} lg={3}>
+                                    <div className="service">
+                                        <div className="service_icon">{service.icon}</div>
+                                        <Typography className="service_title" variant="h6"> {service.title}</Typography>
+                                        <Typography className="service_description" variant="body2">{service.description}</Typography>
+                                    </div>
+                                </Grid>
+                            ))
+                        }
+                    </Grid>
+                </Grid>
+            </Grid>
+            
             {/* skills */}
-            <Grid container="" >
-
+            <Grid container className="section pb_10 pt_25 pl_30" >
+                <Grid item className="all_skills" spacing={4} justifyContent="space-between">
+                    {resumeData.skills.map(skill => (
+                        <Grid item xs={12} sm={12} md={3} className="skill_bottom">
+                            <Paper elevstion={0} className="skill pb_10" >
+                                <Typography variant="h6" className="skill_title">
+                                    {skill.title}
+                                </Typography>
+                                {
+                                    skill.description.map(element => (
+                                        <Typography variant="body2" className="skill_description">
+                                            <TimelineDot variant="outlined" className="timeline_dot">                              
+                                            </TimelineDot>
+                                            <span>{element}</span>
+                                        </Typography>
+                                    ))
+                                }
+                            </Paper>
+                        </Grid>
+                    ))}
+                </Grid>
             </Grid>
+
             {/* contact */}
             <Grid container="" >
 
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -1,31 +1,106 @@
-import { Facebook, Twitter, GitHub } from '@mui/icons-material';
+import { Facebook, Twitter, GitHub, WebOutlined } from "@mui/icons-material";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    name: "Ibnul",
-    title: "Developer",
-    birthday: "13 september 1997",
-    email: "ibnulashir42@gmail.com",
-    address: "6/2 North Adabor",
-    phone: "+8801765427688",
-    socials:{
-        Facebook: {
-            link: 'https://facebook.com',
-            text: "My Facebook",
-            icon: <Facebook />
-        },
-        Twitter: {
-            link: 'https://facebook.com',
-            text: "My Twitter",
-            icon: <Twitter />
-        },
-        Github: {
-            link: 'https://facebook.com',
-            text: "My Github",
-            icon: <GitHub />
-        }
+  name: "Ibnul",
+  title: "Developer",
+  birthday: "13 september 1997",
+  email: "ibnulashir42@gmail.com",
+  address: "6/2 North Adabor",
+  phone: "+8801765427688",
+  socials: {
+    Facebook: {
+      link: "https://facebook.com",
+      text: "My Facebook",
+      icon: <Facebook />,
     },
-    about:
-        `Hi there, I'm Ibnul Ashir. I'm graduated from a university located in Dhaka, Bangladesh.\n
+    Twitter: {
+      link: "https://facebook.com",
+      text: "My Twitter",
+      icon: <Twitter />,
+    },
+    Github: {
+      link: "https://facebook.com",
+      text: "My Github",
+      icon: <GitHub />,
+    },
+  },
+  about: `Hi there, I'm Ibnul Ashir. I'm graduated from a university located in Dhaka, Bangladesh.\n
         My educational background is software engineering. In technologies`,
-}
+  experiences: [
+    {
+      title: "2019",
+      date: "11/12/2019",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas et quae explicabo nisi blanditiis debitis atque perspiciatis, maxime numquam a",
+    },
+    {
+      title: "2019",
+      date: "11/12/2019",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas et quae explicabo nisi blanditiis debitis atque perspiciatis, maxime numquam a",
+    },
+    {
+      title: "2019",
+      date: "11/12/2019",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas et quae explicabo nisi blanditiis debitis atque perspiciatis, maxime numquam a",
+    },
+  ],
+  education: [
+    {
+      title: "Eucation",
+      date: "11/12/2019",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas et quae explicabo nisi blanditiis debitis atque perspiciatis, maxime numquam a",
+    },
+    {
+      title: "Eucation2",
+      date: "11/12/2019",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas et quae explicabo nisi blanditiis debitis atque perspiciatis, maxime numquam a",
+    },
+    {
+      title: "Eucation3",
+      date: "11/12/2019",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas et quae explicabo nisi blanditiis debitis atque perspiciatis, maxime numquam a",
+    },
+  ],
+  services: [
+    {
+      title: 'web developer',
+      description: "I'm a web developer",
+      icon: <WebOutlined />
+    },
+    {
+      title: 'web developer',
+      description: "I'm a web developer",
+      icon: <WebOutlined />
+    },
+    {
+      title: 'web developer',
+      description: "I'm a web developer",
+      icon: <WebOutlined />
+    }
+  ],
+  skills: [
+    {
+      title: 'front-end',
+      description: [
+        "reactjs",
+        "javascript",
+        "Bootstrap",
+      ],      
+    },
+    {
+      title: 'back-end',
+      description: [
+        "NodeJS",
+        "Firebase",
+        "MongoDB",
+      ],      
+    },
+    {
+      title: 'Source Control',
+      description: [
+        "Git",
+        "Github",
+      ],      
+    },
+  ]
+};
